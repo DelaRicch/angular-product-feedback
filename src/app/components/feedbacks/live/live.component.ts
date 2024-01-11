13a4246b0d8d19b8ed89415a98062038188b2056
Dela Ricch <delaricch@gmail.com>
2024-01-11 11:22:42 +0000

    feat: display filtered live feedbacks

--- src/app/components/feedbacks/live/live.component.ts
+++ src/app/components/feedbacks/live/live.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { Feedback } from '@/types';
+import { Store } from '@ngrx/store';
+import { selectFeedback } from '@/app/store/feedback/feedback.selectors';
 
 @Component({
   selector: 'app-live',
@@ -9,5 +12,11 @@ import { CardComponent } from '../card/card.component';
   styleUrl: './live.component.css'
 })
 export class LiveComponent {
+  liveFeedbacks = [] as Feedback[];
 
+  constructor(private store: Store) {
+    this.store.select(selectFeedback).subscribe((feedbacks) => {
+        this.liveFeedbacks = feedbacks.filter((feedback) => feedback.status === 'live');
+       })
+  }
 }
